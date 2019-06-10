1af37483de1fb9fb55b3d11c57b6f28e9d86d73f
MariusA <anca.ticau@gmail.com>
2019-06-10 23:27:38 +0300

    Strings-Ex18

---
+++ Strings-Ex18/Strings-Ex18/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Strings_Ex18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string givenString = "ana are mere";
+            StartsWith(givenString);
+        }
+
+        //Write a method to check whether a string starts with specified characters
+        public static void StartsWith(string input)
+        {
+            char[] specifiedCharacters = { 'a', 'b', 'c' };
+            for (int i = 0; i < specifiedCharacters.Length; i++)
+            {
+                if (input[0] == specifiedCharacters[i])
+                {
+                    Console.WriteLine("Yes, this string stars with a,b or c");
+                }
+            }
+        }
+    }
+}
